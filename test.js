6d65158199c3c875927095c83f0ab75fc410f1dd
Dányi Bence <madbence@gmail.com>
2013-07-28 21:43:24 +0200

    Exaple updated

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var RRD = require('./index.js');
 
 var rrd = new RRD('foo')
-  .step(1)
+  .step('1s')
   .gauge('bar')
   .gauge('foo')
   .average({
@@ -19,20 +19,24 @@ var rrd = new RRD('foo')
 
 var i = 0;
 setInterval(function() {
+  var i = (new Date().getTime()/1000).toFixed(0)
   rrd.update(
-    [(Math.sin(i*Math.PI/180)+1)*50,
-    (Math.cos(i*Math.PI/180*2)+1)*40], function(err) {
+    [(Math.sin(i*Math.PI/180)+1)*5000,
+    Math.random()*10000], function(err) {
       if(err) console.log(err);
       else console.log('Updated!');
     });
-  i++;
 }, 1000);
 
 rrd.graph()
   .from('-10m')
-  .smooth('foo', 100)
+  .smooth('foo', '1m')
     .as('foo_smooth')
-  .line('foo', '#ff0000')
-  .line('bar', '#00ff00')
-  .line('foo_smooth', '#ff000066')
+  .smooth('bar', '2m')
+    .as('bar_smooth')
+  .line('foo', '#ff000044')
+  .line('bar', '#00ff0044')
+  .line('foo_smooth', '#ff0000')
+  .line('bar_smooth', '#00ff00')
+  .table()
   .create('file.png');
