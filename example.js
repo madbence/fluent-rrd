326d17e9453bafadda884d37f35123bb9fe86b96
Dányi Bence <madbence@gmail.com>
2013-07-31 21:10:54 +0200

    Example updated

--- example.js
+++ example.js
@@ -11,7 +11,7 @@ var rrd = new RRD('foo')
   .average({
     resolution: '5s',
     for: '5h'
-  })
+  });
   /*.create(function(err) {
     if(err) console.log(err);
     else console.log('Success!');
@@ -19,22 +19,21 @@ var rrd = new RRD('foo')
 
 var i = 0;
 setInterval(function() {
-  var i = (new Date().getTime()/1000).toFixed(0)
+  var i = (new Date().getTime()/1000).toFixed(0);
   rrd.update(
     [(Math.sin(i*Math.PI/180)+1)*5000,
-    Math.max(5000, (i*100)%10000)], function(err) {
+    Math.max(5000, (i*50)%10000)], function(err) {
       if(err) console.log(err);
       else console.log('Updated!');
     });
 }, 1000);
 
 rrd.graph()
-  .from('-10m')
+  .from('-1h')
   .title('Example graph with random values')
   .watermark('%a')
   .vlabel('Awesomeness')
-  .font('DEFAULT', 8, 'Anonymous Pro')
-  .font('TITLE', 12, 'Anonymous Pro')
+  .size(600, 150)
   .smooth('foo', '10s')
     .as('foo_smooth')
   .smooth('foo_smooth', '1m')
